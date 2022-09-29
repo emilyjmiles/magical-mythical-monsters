7f62681fb4f060b7a9ea789617e0f6ead105536e
Emily Miles <ms.emily.j.miles@gmail.com>
2022-09-29 13:46:52 -0400

    Refactor for readability and accessibility

--- src/main.js
+++ src/main.js
@@ -27,100 +27,104 @@ changeGameButton.addEventListener('click', showMainGameHub);
 restartGameButton.addEventListener('click', clearWinCount);
 
 function hideElements(elements) {
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.add('hidden');
-  }
-}
+  elements.forEach(element => element.classList.add('hidden'));
+};
 
 function showElements(elements) {
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.remove('hidden');
-  }
-}
+  elements.forEach(element => element.classList.remove('hidden'));
+};
 
 function addEventToMonsters(monsterOptions) {
-  for (let i = 0; i < monsterOptions.length; i++) {
-    monsterOptions[i].addEventListener('click', function(event){
+  monsterOptions.forEach(monster =>  {
+    monster.addEventListener('click', function(event){
       playGame(event.target.id, monsterOptions);
-    })
-  }
-}
+    });
+  });
+};
 
 function showMainGameHub() {
   showElements([gameHubView]);
   hideElements([easyGameView, mediumGameView, hardGameView, winnerPageView, changeGameButton, restartGameButton]);
-}
+};
 
 function showEasyGame() {
   showElements([easyGameView, changeGameButton, restartGameButton]);
   hideElements([gameHubView, mediumGameView, hardGameView, winnerPageView]);
-}
+};
 
 function playEasyGame() {
   game.gameChoice = easyGameView;
+
   addEventToMonsters(easyMonsterIcons);
   showEasyGame();
-}
+};
 
 function showMediumGame() {
   showElements([mediumGameView, changeGameButton, restartGameButton]);
   hideElements([gameHubView, easyGameView, hardGameView, winnerPageView]);
-}
+};
 
 function playMediumGame() {
   game.gameChoice = mediumGameView;
+
   addEventToMonsters(mediumMonsterIcons);
   showMediumGame();
-}
+};
 
 function showHardGame() {
   showElements([hardGameView, changeGameButton, restartGameButton]);
   hideElements([gameHubView, easyGameView, mediumGameView, winnerPageView]);
-}
+};
 
 function playHardGame() {
   game.gameChoice = hardGameView;
+
   addEventToMonsters(hardMonsterIcons);
   showHardGame()
-}
+};
 
 function playGame(humanChoice, monsterOptions) {
-  game.human.humanTurn(event.target.id);
+  game.human.humanTurn(humanChoice);
   game.robot.robotTurn(monsterOptions);
   game.checkForWinner(game.human.currentMonster, game.robot.currentMonster);
+
   displayGameResults(game.human.currentMonster, game.robot.currentMonster, game.gameStatus);
   updateWinCount();
   backToGame();
-}
+};
 
 function displayGameResults(humanChoice, robotChoice, gameStatus) {
   showElements([winnerPageView, changeGameButton]);
   hideElements([gameHubView, easyGameView, mediumGameView, hardGameView, changeGameButton, restartGameButton]);
+
   announceWinner.innerHTML = '';
   announceWinner.innerHTML +=
   `<img src="${game.currentWinner}"/>
   <h2>${gameStatus}</h2>
-  <img src="assets/${humanChoice}-icon.png"/><a> VS </> <img src="assets/${robotChoice}-icon.png"/>`
-}
+  <img src="assets/${humanChoice}-icon.png"/><a> VS </> <img src="assets/${robotChoice}-icon.png"/>`;
+};
 
 function updateWinCount() {
   humanWins.innerHTML = `WINS: ${game.human.wins}`;
   robotWins.innerHTML = `WINS: ${game.robot.wins}`;
-}
+};
 
 function backToGame() {
   if (game.gameChoice === easyGameView) {
     setTimeout(showEasyGame, 1500);
+
   } else if (game.gameChoice === mediumGameView) {
     setTimeout(showMediumGame, 1500);
+
   } else if (game.gameChoice === hardGameView) {
     setTimeout(showHardGame, 1500);
-  }
-}
+  };
+};
 
 function clearWinCount() {
   game.human.wins = 0;
   game.robot.wins = 0;
+
   humanWins.innerHTML = `WINS: ${game.human.wins}`;
   robotWins.innerHTML = `WINS: ${game.robot.wins}`;
-}
+};
